ddb2ede367c4d22e8d308e17227cd96e15b0d2fc
drbarq <J.Tustin@gmail.com>
2019-05-08 14:41:25 -0600

    cleaned up environment.rb and turned off SQL logger

--- config/environment.rb
+++ config/environment.rb
@@ -3,24 +3,12 @@ require "pry"
 require "require_all"
 require_all "./app"
 
+ActiveRecord::Base.logger = nil
+
 ActiveRecord::Base.establish_connection({
  :adapter => "sqlite3",
  :database => "./db/millenial_translator.db"
 })
 
-
-# binding.pry
-# Word.create(word: "fam", definition: "family")
+#code starts here
 mta_hello
-#
-# User.create(user_name: "Joe")
-#
-#
-# Favorite.all
-# Favorite.create(favorite_word: "Lit")
-
-# User.find_by(user_name: "Joe")
-# Word.find_by(word: "Lit")
-# Word.find_by(word: "Lit").id
-# Word.find_by(word: "Lit").word
-# Favorite.create(favorite_word: Word.find_by(word: "Lit").word, user_id: User.find_by(user_name: "Joe").id, word_id:  Word.find_by(word: "Lit").id)
