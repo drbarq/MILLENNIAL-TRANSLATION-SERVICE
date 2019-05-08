233f1cb9d4728f164d4f7e9785348623ed8a3923
drbarq <J.Tustin@gmail.com>
2019-05-08 10:18:09 -0600

    fixing favorite functionality

--- config/environment.rb
+++ config/environment.rb
@@ -17,3 +17,9 @@ binding.pry
 # Word.create(word: "Lit", definition: "Very Good")
 # Favorite.all
 # Favorite.create(favorite_word: "Lit")
+
+# User.find_by(user_name: "Joe")
+# Word.find_by(word: "Lit")
+# Word.find_by(word: "Lit").id
+# Word.find_by(word: "Lit").word
+# Favorite.create(favorite_word: Word.find_by(word: "Lit").word, user_id: User.find_by(user_name: "Joe").id, word_id:  Word.find_by(word: "Lit").id)    
